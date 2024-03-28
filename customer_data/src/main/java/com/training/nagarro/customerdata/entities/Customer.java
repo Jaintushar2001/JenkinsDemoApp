dec15bd9fbb872bc88d180513b16053d61230ce4
tusharjain04 <tushar.jain04@nagarro.com>
2024-03-28 12:28:49 +0530

    Initial Commit

---
+++ customer_data/src/main/java/com/training/nagarro/customerdata/entities/Customer.java
@@ -0,0 +1,80 @@
+package com.training.nagarro.customerdata.entities;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "customerId", unique = true)
+	private Long customerId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String gender;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(Long customerId, String name, int age, String email, String gender) {
+		super();
+		this.customerId = customerId;
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.gender = gender;
+	}
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}   
+}
